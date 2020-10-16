00af4fb279b7f3f68be454bdad527bc71d35c868
Kelsea McAllister <kelsea.mcall@gmail.com>
2020-10-16 16:21:46 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,8 +1,5 @@
 def reverse_each_word(string)
   array = string.split
   new_array = []
-  array.collect do |element|
-    new_array << element.reverse
-  end
-  new_array.join(" ")
+  array.collect { |e| e.reverse }
 end
